e6d8c13d199d52ebd1a3ea1d716161fc7761e2cd
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-30 23:11:55 +0900

    feat: todolist 페이지 내 header 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-// function Header() {
-//   return ();
-// }
+function Header() {
+  return (
+    <header>
+      <img src="/logo.png" alt="logo" className="w-28" />
+    </header>
+  );
+}
 
-// export default Header;
+export default Header;
