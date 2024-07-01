93a67557cb0fff149387e118f3bae639d5b275cd
hyunjun9788 <namhyunjun9788@gmail.com>
2024-07-01 15:34:00 +0900

    feat: list, item mockdata 단순 구현

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,10 @@
-const TodoItem = () => <div>TodoItem</div>;
+const TodoItem = () => (
+  <div className="flex gap-4 py-5 border-b-[1px]">
+    <input type="checkbox" />
+    <div className="flex-1">토익</div>
+    <div>날짜</div>
+    <button type="button">삭제</button>
+  </div>
+);
 
 export default TodoItem;
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -4,13 +4,37 @@ import Header from '../components/Header';
 import List from '../components/List';
 import TodoItem from '../components/TodoItem';
 
+interface MockData {
+  id: number;
+  isDone: boolean;
+  content: string;
+  data: number;
+}
+
+const mockData: MockData[] = [
+  { id: 1, isDone: false, content: '토익', data: new Date().getDate() },
+  { id: 2, isDone: false, content: '헬스', data: new Date().getDate() },
+  { id: 3, isDone: false, content: '프로젝트', data: new Date().getDate() },
+];
+
 function TodoList() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(mockData);
+
+  const onCreate = (content: string) => {
+    const newTodo = {
+      id: 4,
+      isDone: false,
+      content,
+      data: new Date().getDate(),
+    };
+    setList([newTodo, ...list]);
+  };
+
   return (
     <div>
       <Header />
       <div className="w-[600px] mt-24 mx-auto">
-        <InputBox setList={setList} />
+        <InputBox onCreate={onCreate} />
         <List />
         <TodoItem />
       </div>
