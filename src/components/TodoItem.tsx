b7465f5592aac6fd310566b38ad6763a0f12b591
hyunjun9788 <namhyunjun9788@gmail.com>
2024-07-01 21:04:40 +0900

    chore: 삭제 버튼 스타일 변경

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,12 +1,22 @@
 import { MockData } from '../types/mockData';
 
-const TodoItem = ({ todo }: { todo: MockData }) => (
-  <div className="flex gap-4 py-5 border-b-[1px]">
-    <input checked={todo.isDone} type="checkbox" />
-    <div className="flex-1">{todo.content}</div>
-    <div>{new Date(todo.date).toLocaleDateString()}</div>
-    <button type="button">삭제</button>
-  </div>
-);
+const TodoItem = ({ todo }: { todo: MockData }) => {
+  const handleClickDeleteBtn = () => {};
+
+  return (
+    <div className="flex gap-4 py-5 border-b-[1px]">
+      <input checked={todo.isDone} type="checkbox" />
+      <div className="flex-1">{todo.content}</div>
+      <div>{new Date(todo.date).toLocaleDateString()}</div>
+      <button
+        type="button"
+        onClick={handleClickDeleteBtn}
+        className="bg-light-blue w-9 text-white text-sm"
+      >
+        삭제
+      </button>
+    </div>
+  );
+};
 
 export default TodoItem;
