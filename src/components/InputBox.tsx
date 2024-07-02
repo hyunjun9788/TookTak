ccabf0b038f548f04b8be797357d05332d8ee4ec
hyunjun9788 <namhyunjun9788@gmail.com>
2024-07-02 20:37:29 +0900

    feat: 엔터 입력 시 빈 값이면 예외처리

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -17,29 +17,34 @@ const InputBox = ({ onCreate }: { onCreate: (content: string) => void }) => {
     setContent(e.target.value);
   };
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    if (e.keyCode === 13) {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      if (content === '' && contentRef.current !== null) {
+        contentRef.current.focus();
+        return;
+      }
       onCreate(content);
+      setContent('');
     }
   };
   return (
-    <form className="flex ">
+    <div className="flex ">
       <input
         ref={contentRef}
         value={content}
         onChange={handleChangeInput}
+        onKeyDown={handleKeyDown}
         placeholder="할 일을 추가해 보세요"
         className="flex-1 border-b-2 p-2 focus:outline-none"
       />
       <button
         type="button"
         onClick={handleClickCreateBtn}
-        onKeyDown={handleKeyDown}
         className="w-14 h-10 rounded-md bg-light-blue text-white"
       >
         추가
       </button>
-    </form>
+    </div>
   );
 };
 
