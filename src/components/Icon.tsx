3499507e4da543f49693575ba7cc8a3e600bcc72
hyunjun9788 <namhyunjun9788@gmail.com>
2024-07-03 16:31:50 +0900

    feat: svgr 플러그인 추가 및 Icon 컴포넌트 구현

---
+++ src/components/Icon.tsx
@@ -0,0 +1,25 @@
+import { HTMLAttributes } from 'react';
+import VisibilityIcon from '../../public/visibility.svg?react';
+import VisibilityOffIcon from '../../public/visibility-off.svg?react';
+
+export type IconType = 'VisibilityIcon' | 'VisibilityOffIcon';
+
+export interface IconProps {
+  name: IconType;
+  className: HTMLAttributes<SVGAElement>['className'];
+  onClick?: React.MouseEventHandler<SVGSVGElement>;
+}
+
+export const iconTypes: Record<
+  IconType,
+  React.ComponentType<React.SVGProps<SVGSVGElement>>
+> = {
+  VisibilityIcon,
+  VisibilityOffIcon,
+};
+
+export const Icon = ({ name, className, onClick }: IconProps) => {
+  const IconComponent = iconTypes[name];
+
+  return <IconComponent className={className} onClick={onClick} />;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import svgr from 'vite-plugin-svgr';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
