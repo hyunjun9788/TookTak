93bf3b4fa450d6e30a99792856e6e2b1ee249901
hyunjun9788 <namhyunjun9788@gmail.com>
2024-07-04 19:54:20 +0900

    feat: 비로그인 시 리다이렉트 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import './App.css';
 import Home from './pages/Home';
 import TodoList from './pages/TodoList';
@@ -7,11 +7,22 @@ import Register from './pages/Register';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
+  const currentUser = false;
+  const RequireAuth = ({ children }: any) => {
+    return currentUser ? children : <Navigate to="/login" />;
+  };
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/todolist" element={<TodoList />} />
+        <Route
+          path="/todolist"
+          element={
+            <RequireAuth>
+              <TodoList />
+            </RequireAuth>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
